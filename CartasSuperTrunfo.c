ecf830a8e8f4407cb0ab82f91a4897a926461172
jogleyson10-ctrl <jogleyson10@gmail.com>
2025-09-25 00:56:30 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,39 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+ 
+    char estado[];
+    char codigodacarta[];
+    char nomedacidade[];
+    int populacao;
+    float areaemkm2;
+    float pib;
+    int numerodepontosturisticos;
+  
   // Área para entrada de dados
+    prinft("o nome do estado é: \n");
+    printf("o nome da carta é: \n");
+    prinft("o nome da cidade é: \n)
+     prinft("o numero da populacao é: \n")
+      prinft("a area em km2 é: \n")
+      prinft(" o pib é: \n")
+      prinft(" o numero de pontos turisticos é: \n")
 
+      
   // Área para exibição dos dados da cidade
 
-return 0;
+    scanf("%s", &estado);
+   scanf("%s", &codigodacarta);
+    scanf("%s", &nomedacidade);
+  scanf("%d", &populacao);
+  scanf("%f", &areakm2);
+  scanf("%f", pib);
+  scanf("%d", numerosdepontosturisticos);
+
+    
+    
+    
+    
+    return 0;
+
 } 
